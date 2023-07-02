b2ee00232633cef1f6e11d5c694b65f94f768348
Sanmitha <sanmithasadanand@gmail.com>
2023-07-02 18:35:15 +0530

    Update form styles

---
+++ action.php
@@ -0,0 +1,9 @@
+<?php 
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $weight = $_POST['weight'];
+    $email = $_POST['email'];
+    $pdf_file = $_POST['pdf_file'];
+
+    //Database connection
+?>
